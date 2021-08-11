844bb028111adc7debbd0d67e96ff35a5981e177
Cisneros-28 <arcacisneros@gmail.com>
2021-08-10 20:09:39 -0400

    views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,28 +13,29 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    /*return view('welcome');*/
-    return "Bienvenido a la pagina principal";
-});
 
-Route::get('cursos', function(){
-    return "Bienvenido a la pagina cursos";
-});
-Route::get('cursos/create', function(){
-    return "En esta pagina podras crear curso";
-});
-/*Route::get('cursos/{curso}', function($curso){
-    return "Bienvenido al curso: $curso";
-});*/
-Route::get('cursos/{curso}/{categoria?}', function($curso,$categoria = null){
+Route::get('/', HomeController::class);
+
+ 
+
+Route::get('cursos', [CursoController::class,'index']);
+
+
+Route::get('cursos/create', [CursoController::class,'create']);
+
+
+Route::get('cursos/{curso}', [CursoController::class,'show']);
+
+
+
+/*Route::get('cursos/{curso}/{categoria?}', function($curso,$categoria = null){
     if($categoria){
         return "Bienvenido al curso: $curso, de la categoria $categoria";
     }else{
         return "Bienvenido al curso: $curso";
     }
     
-});
+});  */
 
 
 
